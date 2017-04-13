bee9a85750e7ee41461afb71253ec9382c401ca3
rob <romijndersrob@gmail.com>
2017-04-13 08:31:16 +0200

    clean working tree

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
-
 import matplotlib
 matplotlib.use('TkAgg')
-from util import input_data
+from util import dataloader
 from model.conv import Model
+from util.util_funcs import EWMA
+import tensorflow as tf  #version 1.1.0rc1
 
-from util.util_funcs import EWMA, acc_intersection
+mnist = dataloader.read_data_sets("MNIST_data",norm=True)
+width, height = mnist.im_size
 
-import numpy as np
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-import tensorflow as tf  #version alpha 1.0
-tf.logging.set_verbosity(tf.logging.ERROR)
+max_iterations = 10000
+log_interval = 100
+bsz = 34
+dropout = 1.0
+cf = {  'width'         :width,
+        'height'        :height,
+        'num_actions'   :10,
+        'lr'            :0.001,
+        'bsz'           :bsz}
 
-mnist = input_data.read_data_sets("MNIST_data")
-
-"""Plot some normal image"""
-cf = {}
-cf['width'] = input_data.MNISTDataSet.width = 14
-cf['height'] =input_data.MNISTDataSet.height =  14
-cf['num_a'] = 1
-cf['num_actions'] = 10 #how many possible actions
-cf['lr'] = 0.001 #Static learning rate
-
-NUM_A = 1
-
-dropout = 0.6
-max_iterations = 100000
-log_interval = 200
-batchsize = input_data.MNISTDataSet.bsz = 54
-
-X, y, r = mnist.train.random_policy()
 model = Model(cf)
+
+#Mess up the policy
+dataloader.pol0 = [0.15, 0.05, 0.15, 0.05, 0.15, 0.05, 0.15, 0.05, 0.15, 0.05]
 
 """Training time"""
 sess = tf.Session()
@@ -38,28 +29,39 @@ sess.run(model.init_op)
 
 track_val = EWMA(0.7)
 track_train = EWMA(0.95)
-track_acc = EWMA(0.7)
+# track_acc = EWMA(0.7)
 
 try:
     print('Start training')
-    print('--Numbers for comparison:')
-    print('  -default strategy yields entropy %5.3f'%(-1.0*np.log(1.0/10)))
-    print('  -optimal strategy yields entropy %5.3f' % (-1.0 * np.log(1.0 / NUM_A)))
+    print('Comparison: a default strategy yields -0.10')
     for i in range(max_iterations):
-        X_batch, y_batch, r_batch = mnist.train.random_policy()
-        cost,_,debug = sess.run([model.MAE,model.train_step,model.pred],{model.X:X_batch, model.y: y_batch,model.reward:r_batch,model.is_train:True, model.keep_prob:dropout})
+        X_batch, y_batch,r_batch,p_batch = mnist.next_lb_batch(bsz)
+        cost,_,debug,db1,db2,db3 = sess.run([model.cost,
+                                            model.train_step,
+                                            model.pol_y,
+                                            model.pi,
+                                            model.pol_y,
+                                            model.ind],
+                                {model.X:           X_batch,
+                                 model.y:           y_batch,
+                                 model.reward:      r_batch,
+                                 model.pol_0:       p_batch,
+                                 model.is_train:    True,
+                                 model.keep_prob:   dropout})
         track_train.add(cost)
 
         if i%log_interval == 0:
-            X_batch, y_batch, r_batch = mnist.train.random_policy()
-            cost,pred = sess.run([model.MAE,model.pred],{model.X:X_batch, model.y:y_batch,model.reward:r_batch,model.is_train:False, model.keep_prob: 1.0})
-            acc = np.mean(np.logical_and(pred > 0.5, r_batch))
-            # if i > 3000:
-            #     for n in range(10):
-            #         print(pred[n], r_batch[n])
+            X_batch, y_batch, r_batch, p_batch = mnist.next_lb_batch(bsz,'val')
+            cost, debug = sess.run([model.cost,
+                                    model.pol_y],
+                                      {model.X: X_batch,
+                                       model.y: y_batch,
+                                       model.reward: r_batch,
+                                       model.pol_0: p_batch,
+                                       model.is_train: False,
+                                       model.keep_prob: dropout})
             track_val.add(cost)
-            track_acc.add(acc)
-            print("At %7i/%7i for train %5.3f/%5.3f for val %5.3f/%5.3f and acc %5.3f/%5.3f "%(i,max_iterations,*track_train.tup,*track_val.tup, *track_acc.tup))
+            print("At %7i/%7i for train %7.3f/%7.3f for val %7.3f/%7.3f"%(i,max_iterations,*track_train.tup,*track_val.tup))
 
 except KeyboardInterrupt:
     print('Finished training')
@@ -67,3 +69,4 @@ finally:
     #Always make sure to close the tensorflow session
     sess.close()
 
+a=0
